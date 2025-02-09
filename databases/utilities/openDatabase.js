553c7524aa747f85627b53c0f38401fe0bfab400
benhuangbmj <benjaminwong1985@gmail.com>
2025-02-09 01:16:05 -0500

    fix: resolve the path for cross-system

--- databases/utilities/openDatabase.js
+++ databases/utilities/openDatabase.js
@@ -1,19 +1,18 @@
 const sqlite3 = require("sqlite3").verbose();
-const rootPath = __dirname.match(/^[\w\W]+backend-lab\//)[0];
-
+const path = require("path");
+const rootPath = path.resolve(__dirname, "..");
 function openDatabase(dbName) {
-  return new sqlite3.Database(
-    `${rootPath}databases/${dbName}.db`,
-    sqlite3.OPEN_READWRITE,
-    (err) => {
-      if (err) {
-        console.log("openDatabase", dbName);
-        console.error(err.message);
-      } else {
-        console.log(`Connected to the ${dbName} database.`);
-      }
-    },
-  );
+	return new sqlite3.Database(
+		path.resolve(rootPath, `${dbName}.db`),
+		sqlite3.OPEN_READWRITE,
+		(err) => {
+			if (err) {
+				console.log("openDatabase", dbName);
+				console.error(err.message);
+			} else {
+				console.log(`Connected to the ${dbName} database.`);
+			}
+		}
+	);
 }
-
 exports.openDatabase = openDatabase;
